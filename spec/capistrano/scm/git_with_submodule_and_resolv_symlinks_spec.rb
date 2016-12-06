63c441cba39a7368930355e7bbeccd41bafe5d07
YAMADA Tsuyoshi <tsu-yamada@groovenauts.jp>
2016-12-06 16:23:05 +0900

    Added test

--- spec/capistrano/scm/git_with_submodule_and_resolv_symlinks_spec.rb
+++ spec/capistrano/scm/git_with_submodule_and_resolv_symlinks_spec.rb
@@ -31,5 +31,104 @@ module Capistrano
         subject.git(:init)
       end
     end
+
+    describe "#repo_mirror_exists?" do
+      it "should call test for repo HEAD" do
+        env.set(:repo_path, "/path/to/repo")
+        backend.expects(:test).with " [ -d /path/to/repo/.git ] "
+
+        subject.repo_mirror_exists?
+      end
+    end
+
+    describe "#check_repo_is_reachable" do
+      it "should test the repo url" do
+        env.set(:repo_url, "url")
+        backend.expects(:execute).with(:git, :'ls-remote', "url", "HEAD").returns(true)
+
+        subject.check_repo_is_reachable
+      end
+    end
+
+    describe "#clone_repo" do
+      it "should run git clone" do
+        env.set(:repo_url, "url")
+        env.set(:repo_path, "path")
+        backend.expects(:execute).with(:git, :clone, "url", "path")
+
+        subject.clone_repo
+      end
+    end
+
+    describe "#update_mirror" do
+      describe "with branch" do
+        it "should run git remote update, git checkout, git submodle update" do
+          env.set(:branch, "real-branch")
+
+          backend.expects(:test).with(:git, :"rev-parse", "real-branch").returns(false)
+          backend.expects(:execute).with(:git, :remote, :update, "--prune")
+          backend.expects(:execute).with(:git, :checkout, "--detach", "origin/real-branch")
+          backend.expects(:execute).with(:git, :submodule, :update, "--init")
+
+          subject.update_mirror
+        end
+      end
+
+      describe "with tag/commit" do
+        it "should run git remote update, git checkout, git submodle update" do
+          env.set(:branch, "tag-or-commit")
+
+          backend.expects(:test).with(:git, :"rev-parse", "tag-or-commit").returns(true)
+          backend.expects(:execute).with(:git, :remote, :update, "--prune")
+          backend.expects(:execute).with(:git, :checkout, "--detach", "tag-or-commit")
+          backend.expects(:execute).with(:git, :submodule, :update, "--init")
+
+          subject.update_mirror
+        end
+      end
+    end
+
+    describe "#release" do
+      it "should run rsync without a subtree" do
+        env.set(:repo_path, "repo-path")
+        env.set(:release_path, "release-path")
+
+        backend.expects(:execute).with("rsync -ar --copy-links --exclude=.git\* repo-path/ release-path")
+
+        subject.release
+      end
+
+      it "should run rsync with a subtree" do
+        env.set(:repo_tree, "tree")
+        env.set(:repo_path, "repo-path")
+        env.set(:release_path, "release-path")
+
+        backend.expects(:execute).with("rsync -ar --copy-links --exclude=.git\* repo-path/tree/ release-path")
+
+        subject.release
+      end
+    end
+
+    describe "#fetch_revision" do
+      describe "with branch" do
+        it "should capture git rev-list" do
+          env.set(:branch, "real-branch")
+          backend.expects(:test).with(:git, :"rev-parse", "real-branch").returns(false)
+          backend.expects(:capture).with(:git, "rev-list", "--max-count=1", "--abbrev-commit", "origin/real-branch").returns("81cec13")
+          revision = subject.fetch_revision
+          expect(revision).to eq("81cec13")
+        end
+      end
+
+      describe "with tag/commit" do
+        it "should capture git rev-list" do
+          env.set(:branch, "tag-or-commit")
+          backend.expects(:test).with(:git, :"rev-parse", "tag-or-commit").returns(true)
+          backend.expects(:capture).with(:git, "rev-list", "--max-count=1", "--abbrev-commit", "tag-or-commit").returns("81cec13")
+          revision = subject.fetch_revision
+          expect(revision).to eq("81cec13")
+        end
+      end
+    end
   end
 end
