d002641a600648d96b7c77937914b85db9094d6c
YAMADA Tsuyoshi <tsu-yamada@groovenauts.jp>
2016-12-07 17:15:59 +0900

    bumped version

--- lib/capistrano/scm/git_with_submodule_and_resolv_symlinks/version.rb
+++ lib/capistrano/scm/git_with_submodule_and_resolv_symlinks/version.rb
@@ -13,7 +13,7 @@ end
 module Capistrano
   class SCM
     class GitWithSubmoduleAndResolvSymlinks < ::Capistrano::SCM::Plugin
-      VERSION = "0.1.0.beta3"
+      VERSION = "0.1.0.beta4"
     end
   end
 end
